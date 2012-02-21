ea530c26575dc437dfebe251e2f34595709dd1eb
jpemberthy <jpemberthy@gmail.com>
2012-02-20 19:02:56 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migue::Application.config.secret_token = '1afb1ee3a14ec5b9d6926f0f4e4e848b16bac6ff7c8da50208b3260b598e2d33099b0c6abe2c474f4d71c86ef91f3bea328fff113bd3b320ece1ec7e4ee35aac'
